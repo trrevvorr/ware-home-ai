9ecf60d6468523dac17b1105c2cd3868513f6e21
Trevor Ross <trevor.is.ross@gmail.com>
2024-05-26 11:13:43 -0400

    fixed service worker registration

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './registerServiceWorker'
-
 const app = createApp(App)
 
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,63 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import { VitePWA } from "vite-plugin-pwa";
+
+const base = "/ware-home-ai/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ware-home-ai/",
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    base,
+    define: {
+        "process.env": {
+            BASE_URL: base,
+        },
+    },
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "WareHome AI",
+                short_name: "WareHome AI",
+                description:
+                    "A personal warehouse management app that helps you keep track of household storage with a specialized LLM!",
+                theme_color: "#000000",
+                background_color: "#000000",
+                icons: [
+                    {
+                        src: "public/assets/manifest-icon-192.maskable.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "public/assets/manifest-icon-192.maskable.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "public/assets/manifest-icon-512.maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "public/assets/manifest-icon-512.maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
